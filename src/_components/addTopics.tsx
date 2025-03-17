92230fdb04039379fc863973b61d1b8d1882e970
Vinicius Juarez <viniciusjuarez720@gmail.com>
2025-03-17 00:02:57 -0300

    Small Changes

--- src/_components/addTopics.tsx
+++ src/_components/addTopics.tsx
@@ -1,10 +1,17 @@
 'use client';
 import { useState } from "react";
-import { HiOutlineMail } from "react-icons/hi";
+import { HiOutlineMail} from "react-icons/hi";
 import { FaLocationDot, FaPlus } from "react-icons/fa6";
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { MdOutlineLibraryBooks } from "react-icons/md";
+import { TbDeviceImacPlus } from "react-icons/tb";
+import { FaRegEyeSlash } from "react-icons/fa6";
+import { TbSettingsPlus } from "react-icons/tb";
+import { FaTrashAlt } from "react-icons/fa";
+import { CiCirclePlus } from "react-icons/ci";
+import { text } from "stream/consumers";
 
 export default function AddTopics() {
   // State
@@ -22,6 +29,8 @@ export default function AddTopics() {
     experienceDescription: "",
     education: "",
     skills: "",
+    languages: "",
+    projects: "",
   });
 
   const [color, setColor] = useState({
@@ -94,13 +103,20 @@ export default function AddTopics() {
     }
   }
 
-
   const [font, setFont] = useState({
     Roboto: "Roboto",
     Poppins: "Poppins",
     Regular: "Regular",
     Arial: "Arial",
     Helvetica: "Helvetica",
+    Verdana: "Verdana",
+    Tahoma: "Tahoma",
+    Georgia: "Georgia",
+    Times: "Times",
+    Courier: "Courier",
+    Impact: "Impact",
+    Comic: "Comic",
+    Lucida: "Lucida",
     sansSerif: "sansSerif",
     monospace: "monospace",
     cursive: "cursive",
@@ -114,6 +130,9 @@ export default function AddTopics() {
     lucidaConsole: "lucidaConsole",
     lucidaSans: "lucidaSans",
     lucidaSansTypewriter: "lucidaSansTypewriter",
+    lucidaTypewriter: "lucidaTypewriter",
+    lucidaHandwriting: "lucidaHandwriting",
+    lucidaCalligraphy: "lucidaCalligraphy"
   })
 
   const [fontFamily, setFontFamily] = useState({
@@ -135,18 +154,155 @@ export default function AddTopics() {
       ...textSize, fontSize: e.target.value
     })
   }
-  console.log(textSize.fontSize)
+  const handleClear = () => {
+    setAbout({
+      about: "",
+      name: "",
+      adress: "",
+      email: "",
+      telephone: "",
+      linkedin: "",
+      github: "",
+      objective: "",
+      experience: "",
+      experienceDate: "",
+      experienceDescription: "",
+      education: "",
+      skills: "",
+      languages: "",
+      projects: "",
+    })
+  }
 
+  const [count2, setCount2] = useState(0)
+  const hiddenClassObj = () => {
+    const objective = document.getElementById("objective");
+    const inputs = objective?.querySelectorAll("input")
+    const txtArea = objective?.querySelectorAll("textarea")
+    const icon2 = document.getElementById("eyeIcon2");
+    if (icon2) {
+    icon2.style.color = "gray";
+    }
+
+    if (count2 === 0) {
+      setCount2(count2 + 1)
+      inputs?.forEach(input => {
+        input.disabled = true
+      })
+      txtArea?.forEach(input => {
+        input.disabled = true
+      })
+    }
+    if (count2 === 1) {
+    inputs?.forEach(input => {
+      input.disabled = false
+      setCount2(count2 - 1)
+    })
+    txtArea?.forEach(input => {
+      input.disabled = false
+    })
+    if (icon2) {
+      icon2.style.color = "black";
+    }
+  }
+}
+  const [count, setCount] = useState(0)
+  const hiddenClassEdu = () => {
+    const education = document.getElementById("education");
+    const inputs = education?.querySelectorAll("input")
+    const txtArea = education?.querySelectorAll("textarea")
+    const icon = document.getElementById("eyeIcon");
+    const container = document.getElementById("containerEdu");
+    if (icon) {
+    icon.style.color = "gray";
+    }
+
+    if (count === 0) {
+      setCount(count + 1)
+      container?.classList.add("hidden")
+      inputs?.forEach(input => {
+        input.disabled = true
+      })
+      txtArea?.forEach(input => {
+        input.disabled = true
+      })
+    }
+    if (count === 1) {
+    inputs?.forEach(input => {
+      input.disabled = false
+      setCount(count - 1)
+      container?.classList.remove("hidden")
+    })
+    txtArea?.forEach(input => {
+      input.disabled = false
+    })
+    if (icon) {
+      icon.style.color = "black";
+    }
+  }
+}
+
+const [countLinkd, setCountLinkd] = useState(0)
+const handleDeleteLinkd = () => {
+  const LinkedinElement = document.getElementById("linkedin");
+  const addLinkedinCard = document.getElementById("addLinkedinCard");
+  if (LinkedinElement) {
+  LinkedinElement.style.display = "none";
+  about.linkedin = ""
+  }
+  if (addLinkedinCard) {
+    addLinkedinCard.style.display = "flex";
+  }
+  setCountLinkd(countLinkd + 1)
+}
+function handleAddLinkedin() {
+  const LinkedinElement = document.getElementById("linkedin");
+  const addLinkedinCard = document.getElementById("addLinkedinCard");
+  if (LinkedinElement) {
+  LinkedinElement.style.display = "flex";
+  }
+  if (addLinkedinCard) {
+    addLinkedinCard.style.display = "none";
+  }
+  setCountLinkd(countLinkd - 1)
+}
+
+const [countGit, setCountGit] = useState(0)
+const handleDeleteGit = () => {
+  const GithubElement = document.getElementById("github");
+  const addGithubCard = document.getElementById("addGithubCard");
+  if (GithubElement) {
+  GithubElement.style.display = "none";
+  about.github = ""
+  }
+  if (addGithubCard) {
+    addGithubCard.style.display = "flex";
+  }
+  setCountGit(countGit + 1)
+}
+function handleAddGit() {
+  const GithubElement = document.getElementById("github");
+  const addGithubCard = document.getElementById("addGithubCard");
+  if (GithubElement) {
+  GithubElement.style.display = "flex";
+  }
+  if (addGithubCard) {
+    addGithubCard.style.display = "none";
+  }
+  setCountGit(countGit - 1)
+}
+
+  
     return (
         <>
-        <div className="flex w-screen h-screen">
-        <section className="flex flex-col w-1/2 h-screen justify-center items-center overflow-y-scroll">
-          <div className="bg-white flex flex-col w-9/10 h-auto border border-gray-500 justify-center items-center mt-100 mb-2 rounded-md">
+        <div className="flex w-screen h-auto">
+        <section id="main" className="flex flex-col w-1/2 max-h-[100vh] pt-50 justify-center items-center overflow-y-scroll">
+          <div id="about" className="bg-white flex flex-col w-9/10 h-auto border border-gray-500 justify-center items-center mb-2 rounded-md mt-130">
             <input
             onChange={handleChange}
             type="text"
             name="about"
-            defaultValue={"About me"}
+            defaultValue={"About me:"}
             className="w-9/10 h-12 text-2xl font-bold border-b mb-5 pl-5 outline-0 border-gray-300 text-gray-400"
              />
             <input
@@ -179,22 +335,38 @@ export default function AddTopics() {
             className="w-3/10 h-12 border mb-5 pl-5 rounded-md outline-0 border-gray-300 placeholder:text-gray-400 text-gray-500"
             />
             </div>
+            <div>
+              <CiCirclePlus onClick={handleAddLinkedin} id="addLinkedinCard" size={30} color="gray" style={{display: "none", marginBottom: "10px"}}/>
+            </div>
+            <div id="linkedin" className="flex w-9/10 h-12 mb-5">
             <input
             onChange={handleChange}
             type="text"
             name="linkedin"
             placeholder="Enter your LinkedIn"
-            className="w-9/10 h-12 border mb-5 pl-5 rounded-md outline-0 border-gray-300 placeholder:text-gray-400 text-gray-500"
+            className="w-10/10 h-12 border mb-5 pl-5 rounded-md outline-0 border-gray-300 placeholder:text-gray-400 text-gray-500"
             />
+            <FaTrashAlt id="deleteIcon" onClick={handleDeleteLinkd} style={{position: "relative", right: "40px", top: "13px"}} size={25} color="gray" />
+            </div>
+            <div>
+              <CiCirclePlus onClick={handleAddGit} id="addGithubCard" size={30} color="gray" style={{display: "none", marginBottom: "10px"}}/>
+            </div>
+            <div id="github" className="flex w-9/10 h-12 mb-5">
             <input
             onChange={handleChange}
             type="text"
             name="github"
             placeholder="Enter your Github"
-            className="w-9/10 h-12 border mb-5 pl-5 rounded-md outline-0 border-gray-300 placeholder:text-gray-400 text-gray-500"
+            className="w-10/10 h-12 border mb-5 pl-5 rounded-md outline-0 border-gray-300 placeholder:text-gray-400 text-gray-500"
             />
+            <FaTrashAlt onClick={handleDeleteGit} style={{position: "relative", right: "40px", top: "13px"}} size={25} color="gray" />
+            </div>
           </div>
-          <div className="flex flex-col pt-5 w-9/10 h-auto justify-center items-center mt-10 mb-20 rounded-md flex-grow placeholder:text-gray-400 text-gray-500">
+          <div id="objective" className="flex flex-col border border-gray-400 pt-5 w-9/10 h-auto justify-center items-center mt-10 mb-10 rounded-md flex-grow placeholder:text-gray-400 text-gray-500">
+          <div className="flex justify-between">
+          <TbDeviceImacPlus size={30} color="gray" className="relative right-60 mb-5" />
+        <FaRegEyeSlash id="eyeIcon2" onClick={hiddenClassObj} size={30} color="black" className="relative left-60"/>
+          </div>
             <input
               onChange={handleChange}
               type="text"
@@ -236,6 +408,12 @@ export default function AddTopics() {
                 id="txtArea"
               />
             </div>
+            </div>
+            <div id="education" className="flex flex-col border border-gray-400 pt-5 w-9/10 h-auto justify-center items-center mt-10 mb-20 rounded-md flex-grow placeholder:text-gray-400 text-gray-500">
+            <div className="flex justify-between mb-5">
+            <MdOutlineLibraryBooks size={30} color="gray" className="relative right-60" />
+            <FaRegEyeSlash id="eyeIcon" onClick={hiddenClassEdu} size={30} color="black" className="relative left-60"/>
+            </div>
             <input
             onChange={handleChange}
             type="text"
@@ -258,6 +436,20 @@ export default function AddTopics() {
             placeholder="Your skills"
             className="w-9/10 h-12 border-2 mb-5 pl-5 rounded-md outline-0 border-gray-300 placeholder:text-gray-400 text-gray-500">
             </textarea>
+            <input
+            type="text"
+            onChange={handleChange}
+            name="languages"
+            placeholder="Your languages"
+            className="w-9/10 h-12 border mb-5 pl-5 rounded-md outline-0 border-gray-300 placeholder:text-gray-400 text-gray-500"
+            />
+            <input
+            type="text"
+            onChange={handleChange}
+            name="projects"
+            placeholder="Your projects"
+            className="w-9/10 h-12 border mb-5 pl-5 rounded-md outline-0 border-gray-300 placeholder:text-gray-400 text-gray-500"
+            />
           </div>
         </section>
         <div className=" w-1/2 h-screen fixed top-15 left-1/2">
@@ -280,71 +472,72 @@ export default function AddTopics() {
             />
         </div>
         </div>
-            <div style={{ fontFamily: fontFamily.fontFamily, fontSize: textSize.fontSize }} className="flex flex-col mt-10 ml-30 flex-wrap w-[21.59rem] h-[29.7rem] shadow-lg items-center">
+            <div style={{ fontFamily: fontFamily.fontFamily, fontSize: textSize.fontSize }} id="cv" className="flex flex-col mt-10 ml-30 flex-wrap w-[21.59rem] h-[29.7rem] shadow-lg items-center">
               <div style={{ backgroundColor: color.color1 }} className="h-[29rem] w-[8px] mt-1"></div>
               <div className="h-auto mt-3 w-9/10 items-center">
                   <p style={{ color: textColor.colorText }} className="text-1xl font-bold">{about.name}</p>
                   <div className="flex justify-between flex-wrap">
                   <div className="flex items-center">
                   {renderLocation()}
-                  <p className="ml-1 text-[0.6rem]">{about.adress}</p>
+                  <p className="ml-1 text-[0.5rem]">{about.adress}</p>
                   </div>
                   <div className="flex">
                   {renderEmail()}
-                  <p className="ml-1 text-[0.6rem]">{about.email}</p>
+                  <p className="ml-1 text-[0.5rem]">{about.email}</p>
                   </div>
                   <div className="flex items-center">
                   {renderPhone()}
-                  <p className="ml-1 text-[0.6rem]">{about.telephone}</p>
+                  <p className="ml-1 text-[0.5rem]">{about.telephone}</p>
                   </div>
                   <div className="flex items-center">
                   {renderLinkedin()}
-                  <p className="ml-1 text-[0.6rem]">{about.linkedin}</p>
+                  <p className="ml-1 text-[0.5rem]">{about.linkedin}</p>
                   </div>
                   <div className="flex items-center">
                   {renderGithub()}
-                  <p className="ml-1 text-[0.6rem]">{about.github}</p>
+                  <p className="ml-1 text-[0.5rem]">{about.github}</p>
                   </div>
                   </div>
                   <div className="flex flex-col justify-between">
                   <div className="flex items-center">
                   <div style={{ backgroundColor: color.color1 }} className="mt-3 h-[1rem] w-[5px] mr-1"></div>
-                  <p style={{ color: textColor.colorText }} className="mt-3 text-[0.8rem] font-bold">Objective</p>
+                  <p style={{ color: textColor.colorText }} className="mt-3 text-[0.7rem] font-bold">Objective</p>
                   </div>
-                  <p className="text-[0.6rem]">{about.objective}</p>
+                  <p className="text-[0.5rem]">{about.objective}</p>
                   <div className="flex items-center">
                   <div style={{ backgroundColor: color.color1 }} className="mt-3 h-[1rem] w-[5px] mr-1"></div>
-                  <p style={{ color: textColor.colorText }} className="mt-3 text-[0.8rem] font-bold">Experience</p>
+                  <p style={{ color: textColor.colorText }} className="mt-3 text-[0.7rem] font-bold">Experience</p>
                   </div>
                   <div className="flex-wrap">
                   <div className="flex justify-between">
-                  <p className="text-[0.6rem]">{about.experience}</p>
-                  <p className="text-[0.6rem]">{about.experienceDate}</p>
+                  <p className="text-[0.5rem]">{about.experience}</p>
+                  <p className="text-[0.5rem]">{about.experienceDate}</p>
                   </div>
                   </div>
                   <div className="flex flex-col ml-3 w-auto">
                     <ul>
                     {about.experienceDescription.split('\n').map((item, index) => (
-                    <li key={index} className="text-[0.6rem] flex items-center">
+                    <li key={index} className="text-[0.5rem] flex items-center">
                     <span className="mr-1">•</span>
                     {item}
                     </li>
                     ))}
                     </ul>
                   </div>
+                  <div id="containerEdu">
                   <div className="flex items-center">
                   <div style={{ backgroundColor: color.color1 }} className="mt-3 h-[1rem] w-[5px] mr-1"></div>
-                  <p style={{ color: textColor.colorText }} className="mt-3 text-[0.8rem] font-bold">Education</p>
+                  <p style={{ color: textColor.colorText }} className="mt-3 text-[0.7rem] font-bold">Education</p>
                   </div>
-                  <p className="text-[0.6rem]">{about.education}</p>
+                  <p className="text-[0.5rem]">{about.education}</p>
                   <div className="flex items-center">
                   <div style={{ backgroundColor: color.color1 }} className="mt-3 h-[1rem] w-[5px] mr-1"></div>
-                  <p style={{ color: textColor.colorText }} className="mt-3 text-[0.8rem] font-bold">Skills</p>
+                  <p style={{ color: textColor.colorText }} className="mt-3 text-[0.7rem] font-bold">Skills</p>
                   </div>
                   <div className="flex flex-col ml-3 w-auto">
                     <ul>
                     {about.skills.split('\n').map((item, index) => (
-                    <li key={index} className="text-[0.6rem] flex items-center">
+                    <li key={index} className="text-[0.5rem] flex items-center">
                     <span className="mr-1">•</span>
                     {item}
                     </li>
@@ -353,20 +546,41 @@ export default function AddTopics() {
                   </div>
                   <div className="flex items-center">
                   <div style={{ backgroundColor: color.color1 }} className="mt-3 h-[1rem] w-[5px] mr-1"></div>
-                  <p style={{ color: textColor.colorText }} className="mt-3 text-[0.8rem] font-bold">Languages</p>
+                  <p style={{ color: textColor.colorText }} className="mt-3 text-[0.7rem] font-bold">Languages</p>
                   </div>
-                  <p className="text-[0.6rem]">{}</p>
+                  <p className="text-[0.5rem]">{about.languages}</p>
+                  <div className="flex items-center">
+                  <div style={{ backgroundColor: color.color1 }} className="mt-3 h-[1rem] w-[5px] mr-1"></div>
+                  <p style={{ color: textColor.colorText }} className="mt-3 text-[0.7rem] font-bold">Projects</p>
+                  <div>
+                  </div>
+                  </div>
+                  <p className="text-[0.5rem]">{about.projects}</p>
+                  </div>
                   </div>
               </div>
             </div>
+            <div>
+            </div>
             <div className="fixed top-[90px] right-0 h-[100vh] w-[10rem]">
             <div className="flex flex-col items-center h-4/10">
+            <div className="flex flex-col items-center">
             <p className="text-1xl">Color Lines</p>
             <input onChange={handleColorChange} name="color1" type="color" id="color" value={color.color1} className="color-input w-[50px] h-[50px] cursor-pointer" />
+            <input onChange={handleColorChange} style={{ color: color.color1, borderColor: color.color1 }} name="color1" defaultValue={color.color1} type="text" className="w-5/10 border-b outline-0" />
+            </div>
+            <div className="flex flex-col items-center">
             <p className="text-1xl">Color Text</p>
             <input onChange={handleColorChange} name="colorText" type="color" id="color" value={textColor.colorText} className="color-input w-[50px] h-[50px] cursor-pointer" />
+            <input onChange={handleColorChange} style={{ color: textColor.colorText, borderColor: textColor.colorText }} name="colorText" defaultValue={textColor.colorText} type="text" className="w-5/10 border-b outline-0" />
             </div>
             </div>
+            </div>
+            <section className="mt-5 w-10/10 h-12">
+                <button className="w-[100px] h-[40px] border-3 border-green-400 rounded-md float-end mr-5 font-bold text-green-400 cursor-pointer">Download</button>
+                <button className="w-[100px] h-[40px] border-3 border-blue-400 rounded-md float-end mr-5 font-bold text-blue-500 cursor-pointer">Preview</button>
+                <button onClick={handleClear} className="w-[100px] h-[40px] border-3 border-red-400 rounded-md float-end mr-5 font-bold text-red-400 cursor-pointer">Clear</button>
+            </section>
         </div>
         </div>
         </>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,11 @@
 export default function Header() {
     return (
         <>
-            <div className="bg-white flex justify-between items-center border border-gray-400 w-screen h-[80px]">
-                <h1 className="text-4xl font-bold ml-5 mt-10">CvMaker</h1>
+            <div className="bg-blue-400 text-white flex justify-between items-center border border-gray-400 w-screen h-[50px]">
+                <h1 className="text-4xl font-bold ml-5">CvMaker</h1>
                 <div className="userLog flex mr-3">
-                <h3 className="mr-3 mt-10">Login</h3>
-                <h3 className="mt-10">Register</h3>
+                <h3 className="mr-3">Login</h3>
+                <h3 className="">Register</h3>
                 </div>
             </div>
         </>
